1dd52e47e14428846868948c65a626751fb793fd
kris <kris@lithium.(none)>
2011-02-03 22:21:36 +0100

    adapted debian backend to use templates

--- builder/debian/__init__.py
+++ builder/debian/__init__.py
@@ -14,68 +14,66 @@ locale.setlocale(locale.LC_ALL, 'en_US.UTF-8')
 
 class DebianPackage(builder.package.BasePackage):
 
-    def __init__(self, pkg_name, pkg_dest):
+    def __init__(self, src, dest, jinja_env):
 
-        builder.package.BasePackage.__init__(self, pkg_name, pkg_dest)
+        builder.package.BasePackage.__init__(self, src, dest, jinja_env)
 
         self.pkg_info['timestamp'] = time.strftime('%a, %d %b %Y %H:%M:%S +0100')
-        self.pkg_name = pkg_name
-
-        self.files = [
-            'changelog',
-            'control',
-            'copyright',
-            'rules'
-        ]
-
-    def prepare_build_tree(self):
-
-        pkg_name = '{0}-{1}-{2}'.format(self.pkg_info['name'],
+        
+        self.pkg_name = '{0}-{1}-{2}'.format(self.pkg_info['name'],
                                         self.pkg_info['version'],
                                         self.pkg_info['release']
         )
 
-        src = os.path.join(builder.common.SRC_DIR, self.pkg_name, builder.helper.guess_distribution())
-        tmp = tempfile.mkdtemp(prefix='cream-builder-')
-        package_dir = os.path.join(tmp, self.pkg_name)
-        build_dir = os.path.join(tmp, self.pkg_name, pkg_name)
+        self.files = [
+            'debian/control',
+            'debian/rules',
+            'debian/copyright',
+            'debian/changelog'
+        ]
 
-        os.mkdir(package_dir)
+
+    def prepare_build_tree(self):
+
+        os.mkdir(self.dest) 
 
         # download tarball
         tarball = self.pkg_info['source']
-        tarball = tarball.replace('$pkgname-$pkgver-$pkgrel', pkg_name)
-        target = os.path.join(package_dir, tarball.split('/')[-1])
+        tarball = tarball.replace('$pkgname-$pkgver-$pkgrel', self.pkg_name)
+        target = os.path.join(self.dest, tarball.split('/')[-1])
         urllib.urlretrieve(tarball, target)
 
         # unpack archive
-        p = subprocess.Popen(['tar', '-xf', target, '--directory', package_dir])
+        p = subprocess.Popen(['tar', '-xf', target, '--directory', self.dest])
         ret = os.waitpid(p.pid, 0)[1]
         if ret != 0:
             raise Exception
 
+
+        build_dir = os.path.join(self.dest, self.pkg_name)
         os.mkdir(os.path.join(build_dir, 'debian'))
         for file_ in self.files:
-            shutil.copy(os.path.join(src, file_), os.path.join(build_dir, 'debian', file_))
-        os.chdir(build_dir)
+            shutil.copy(os.path.join(self.src, file_), os.path.join(build_dir, file_))
 
-        return package_dir, build_dir
+        return build_dir
 
 
     def build(self):
 
-        package_dir, build_dir = self.prepare_build_tree()
-
+        build_dir = self.prepare_build_tree()
+        
+        os.chdir(self.dest)
         for file_ in self.files:
-            path = os.path.join(build_dir, 'debian', file_)
+            path = os.path.join(self.pkg_name, file_)
             self.process_template(path)
 
+        os.chdir(build_dir)
         p = subprocess.Popen(['debuild', '--no-tgz-check'])
         ret = os.waitpid(p.pid, 0)[1]
 
-        for i in os.listdir(package_dir):
+        for i in os.listdir(self.dest):
             if i.endswith('.deb'):
-                package_path = os.path.join(build_dir, i)
+                package_path = os.path.join(self.dest, i)
 
         if ret != 0:
             return False
